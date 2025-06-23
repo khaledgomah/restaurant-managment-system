d813fcfe003a06016a716b7bc4a80ab0bb7fad9b
khaledgomah <khaledgomah10@gmail.com>
2025-06-23 20:40:25 +0300

    create online order usecases

---
+++ lib/features/online_order/domain/usecases/usecases.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:restaurant_system/core/errors/failure.dart';
+import 'package:restaurant_system/core/use_case/use_case.dart';
+import 'package:restaurant_system/features/online_order/data/models/online_order.dart';
+import 'package:restaurant_system/features/online_order/domain/repositories/repository.dart';
+
+class GetCompletedOrdersUsecase implements UseCase<List<OnlineOrder>, NoParams> {
+  final OnlineOrderRepository onlineOrderRepository;
+  GetCompletedOrdersUsecase(this.onlineOrderRepository);
+  @override
+  Future<Either<Failure, List<OnlineOrder>>> call(NoParams params) async {
+    return await onlineOrderRepository.getCompletedOrders();
+  }
+}
+class GetPendingOrdersUsecase implements UseCase<List<OnlineOrder>, NoParams> {
+  final OnlineOrderRepository onlineOrderRepository;
+  GetPendingOrdersUsecase(this.onlineOrderRepository);
+  @override
+  Future<Either<Failure, List<OnlineOrder>>> call(NoParams params) async {
+    return await onlineOrderRepository.getPendingOrders();
+  }
+}
+
+class CompleteOrderUsecase implements UseCase<void, String> {
+  final OnlineOrderRepository onlineOrderRepository;
+  CompleteOrderUsecase(this.onlineOrderRepository);
+  
+  @override
+  Future<Either<Failure, void>> call(String orderId) async {
+    return await onlineOrderRepository.completeOrder(orderId);
+  }
+}
